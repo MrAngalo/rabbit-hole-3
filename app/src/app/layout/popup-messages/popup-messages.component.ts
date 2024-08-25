6d10f16cefeceaba984a293bb1fa795bfff73bec
MrAngalo <guilherme.carvalho7@hotmail.com>
2024-08-25 14:16:36 -0400

    displaying messages from popup service

--- app/src/app/layout/popup-messages/popup-messages.component.ts
+++ app/src/app/layout/popup-messages/popup-messages.component.ts
@@ -1,10 +1,21 @@
 import { Component } from "@angular/core";
+import { PopupMessagesService } from "../../services/popup-messages/popup-messages.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "app-popup-messages",
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: "./popup-messages.component.html",
     styleUrl: "./popup-messages.component.scss"
 })
-export class PopupMessagesComponent {}
+export class PopupMessagesComponent {
+    colorMap = {
+        blue: "#56a9f2",
+        green: "#0B950B",
+        yellow: "#A27114",
+        red: "#F54747"
+    };
+
+    constructor(public popup: PopupMessagesService) {}
+}
--- app/src/app/services/popup-messages/popup-messages.service.ts
+++ app/src/app/services/popup-messages/popup-messages.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Subject } from "rxjs";
+
+export type PopupInfo = {
+    message: string;
+    color: "green" | "red" | "yellow" | "blue";
+};
 
 @Injectable({
     providedIn: "root"
 })
 export class PopupMessagesService {
+    private popups: Set<PopupInfo> = new Set();
+    private popupSubject = new BehaviorSubject(this.popups);
+    popups$ = this.popupSubject.asObservable();
+
     constructor() {}
+
+    display(popup: PopupInfo) {
+        this.popups.add(popup);
+    }
+
+    clear() {
+        this.popups.clear();
+    }
 }
