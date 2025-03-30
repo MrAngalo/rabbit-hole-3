b3a819fa2a009db533ca8d8f46cc06907768677b
MrAngalo <guilherme.carvalho7@hotmail.com>
2025-03-30 14:01:28 -0400

    moved apps to ./apps

--- api/core/settings.py
+++ api/core/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import sys
 from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 
 
 # Quick-start development settings - unsuitable for production
