8b0bf068556dbb5f30362e10d4d5006fbdf19955
Gui <gsilva@bitzerus.com>
2025-06-12 22:47:31 -0400

    cloned user page to account

--- app/src/app/pages/account/account.component.ts
+++ app/src/app/pages/account/account.component.ts
@@ -1,10 +1,36 @@
 import { Component } from "@angular/core";
+import { UtilsPipeModule } from "../../pipes/utils/utils-pipe.module";
+import { RouterModule } from "@angular/router";
+import { TenorPipesModule } from "../../pipes/tenor/tenor-pipes.module";
+import { CommonModule } from "@angular/common";
+import { AuthService } from "../../services/auth/auth.service";
+import { mergeMap, Observable } from "rxjs";
+import { User } from "../../services/auth/auth-types";
+import {
+    FetchUserResponse,
+    UserPremission
+} from "../../services/user/user-types";
+import { UserService } from "../../services/user/user.service";
 
 @Component({
     selector: "app-account",
     standalone: true,
-    imports: [],
+    imports: [CommonModule, RouterModule, TenorPipesModule, UtilsPipeModule],
     templateUrl: "./account.component.html",
     styleUrl: "./account.component.scss"
 })
-export class AccountComponent {}
+export class AccountComponent {
+    UserPremission = UserPremission;
+    reqUser$: Observable<FetchUserResponse>;
+
+    currentUser$: Observable<User | null>;
+
+    constructor(auth: AuthService, userService: UserService) {
+        this.currentUser$ = auth.user$;
+        this.reqUser$ = this.currentUser$.pipe(
+            mergeMap((user) => userService.fetchUser(user!.username))
+        );
+    }
+
+    // Save form action="/modify/usersettings/<%= locals.user2.username %>" method="POST"
+}
