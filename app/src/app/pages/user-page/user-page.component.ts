8b41c62159a51373effa11f10df1702d6ba417f3
Gui <gsilva@bitzerus.com>
2025-06-12 23:21:57 -0400

    split user-page and account page

--- app/src/app/pages/user-page/user-page.component.ts
+++ app/src/app/pages/user-page/user-page.component.ts
@@ -3,9 +3,7 @@ import { UtilsPipeModule } from "../../pipes/utils/utils-pipe.module";
 import { ActivatedRoute, Router, RouterModule } from "@angular/router";
 import { TenorPipesModule } from "../../pipes/tenor/tenor-pipes.module";
 import { CommonModule } from "@angular/common";
-import { AuthService } from "../../services/auth/auth.service";
 import { mergeMap, Observable } from "rxjs";
-import { User } from "../../services/auth/auth-types";
 import {
     FetchUserResponse,
     UserPremission
@@ -24,17 +22,12 @@ export class UserPageComponent {
     UserPremission = UserPremission;
     reqUser$: Observable<FetchUserResponse>;
 
-    currentUser$: Observable<User | null>;
-
     constructor(
         router: Router,
         route: ActivatedRoute,
-        auth: AuthService,
         userService: UserService,
         popupService: PopupMessagesService
     ) {
-        this.currentUser$ = auth.user$;
-
         this.reqUser$ = route.params.pipe(
             mergeMap((params) => userService.fetchUser(params["username"]))
         );
